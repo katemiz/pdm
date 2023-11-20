0b38a7ffa260c351e2a13b4179d0f9eb061218b3
Kılıç Ali Temiz <katemiz@gmail.com>
2023-11-20 16:18:17 +0300

    hero svg updated

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,14 +8,14 @@
                 <p class="title has-text-weight-light is-size-1">Be Agile<br>Run Agile</p>
                 <p class="subtitle is-size-4">{{ config('appconstants.app.name') }}</p>
 
-                <figure class="image my-4">
+                {{-- <figure class="image my-4">
                     <img src="images/masttech_pdm.svg" alt="Company PDM">
-                </figure>
+                </figure> --}}
             </div>
 
             <div class="column">
                 <figure class="image my-0 mx-6">
-                    <img src="images/hero.svg" alt="relevant illusturation">
+                    <img src="images/pdmhero.svg" alt="PDM Hero">
                 </figure>
             </div>
         </div>

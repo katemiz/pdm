5e724d520076b3cde5ae241647b7a7dab3b5b60a
Kılıç Ali Temiz <katemiz@gmail.com>
2024-06-14 13:36:28 +0300

    Corrected Icon Title for Parts By Material

--- change_icon_titles.sh
+++ change_icon_titles.sh
@@ -5,6 +5,4 @@ sed -i 's/<title>trash-can<\/title>/<title>Delete Item<\/title>/g' ./vendor/code
 sed -i 's/<title>send<\/title>/<title>Release Item<\/title>/g' ./vendor/codeat3/blade-carbon-icons/resources/svg/send.svg
 sed -i 's/<title>stamp<\/title>/<title>Freeze Item<\/title>/g' ./vendor/codeat3/blade-carbon-icons/resources/svg/stamp.svg
 sed -i 's/<title>document--pdf<\/title>/<title>Convert to PDF<\/title>/g' ./vendor/codeat3/blade-carbon-icons/resources/svg/document-pdf.svg
-sed -i 's/<title>document--pdf<\/title>/<title>Parts with Material<\/title>/g' ./vendor/codeat3/blade-carbon-icons/resources/svg/connect-reference.svg
-
-
+sed -i 's/<defs><\/defs>/<defs><\/defs><title>Parts with Material<\/title>/g' ./vendor/codeat3/blade-carbon-icons/resources/svg/connect-reference.svg

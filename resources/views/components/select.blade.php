b117288cd31a79b3d102c2af7d4b8583213530d8
Kılıç Ali Temiz <katemiz@gmail.com>
2024-12-11 21:59:26 +0300

    continued working on materials

--- resources/views/components/select.blade.php
+++ resources/views/components/select.blade.php
@@ -1,45 +1,13 @@
-{{-- <div class="flex flex-col my-4">
-
-    <h3 class="font-semibold text-gray-900 text-md mb-2">{{ $label }}</h3>
-
-    <div class="flex flex-col md:flex-row gap-4">
-
-
-        @foreach ($options as $value => $title)
-            <div class="">
-                <input type="radio" id="{{ $name . '_' . $value }}" name="{{ $name }}" value="{{ $value }}"
-                    {{ $selected == $value ? 'checked' : '' }} class="form-check-input"
-                    {{ $attributes->merge(['class' => 'form-check-input']) }}>
-                <label for="{{ $name . '_' . $value }}" class="text-base ml-0.5">
-                    {{ $title }}
-                </label>
-            </div>
-        @endforeach
-
-        
-    </div>
-
-    <div class="text-red-600 my-1 font-bold">
-        @error('form.' . $name)
-            <span class="error">{{ $message }}</span>
-        @enderror
-    </div>
-</div> --}}
-
-
-
-
-
 <div class="flex flex-col my-4">
 
     <h3 class="font-semibold text-gray-900 text-md mb-2">{{ $label }}</h3>
 
     <div class="flex flex-col md:flex-row gap-4">
-        <select wire:model='family' class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            <option>Select Family</option>
+        <select class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option>{{ $label }}</option>
 
             @foreach ($options as $key => $value)
-                <option value="{{$key}}">{{$value}}</option>
+                <option value="{{$key}}" {{ $selected == $key ? 'selected' : '' }}>{{$value}}</option>
             @endforeach
         </select>
     </div>
@@ -51,19 +19,3 @@
     </div>
 
 </div>
-
-
-
-
-{{-- 
-<form class="max-w-sm mx-auto">
-  <label for="countries" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an option</label>
-  <select id="countries" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-    <option selected>Choose a country</option>
-    <option value="US">United States</option>
-    <option value="CA">Canada</option>
-    <option value="FR">France</option>
-    <option value="DE">Germany</option>
-  </select>
-</form> --}}
-
--- resources/views/materials/form.blade.php
+++ resources/views/materials/form.blade.php
@@ -2,9 +2,9 @@
 
     <livewire:header
         type="Hero"
-        title="{{ $this->form->uid ? config('conf_documents.form_edit.title') : config('conf_documents.form_add.title') }}"
-        subtitle="{{ $this->form->uid ? config('conf_documents.form_edit.subtitle') : config('conf_documents.form_add.subtitle') }}"
-        />
+        title="{{ $this->form->uid ? $conf['formEdit']['title'] : $conf['formCreate']['title'] }}"
+        subtitle="{{ $this->form->uid ? $conf['formEdit']['subtitle'] : $conf['formCreate']['subtitle'] }}"
+    />
 
     <form wire:submit="{{ $this->form->uid ? 'update' : 'save' }}" action="post">
 
@@ -18,24 +18,41 @@
         <h1 class="text-5xl font-light my-6">{{ $this->form->uid }}</h1>
         @endif
 
-        {{-- <x-radio label="Select Company" name="company_id" :options="$this->form->companies" :selected="$this->form->company_id"
-            wire:model="form.company_id" /> --}}
+        <div class="grid grid-cols-3 grid-rows-1 gap-4">
+            <div >
+                <x-select
+                    label="Material Family"
+                    name="family"
+                    :options="$conf['families']"
+                    :selected="$this->form->family"
+                    wire:model="form.family"
+                />
+            </div>
+            <div >
+                <x-input-text
+                    wire:model="form.description"
+                    name="title"
+                    label="Material Description/Name"
+                    placeholder="Enter material name/description ..."
+                />
+            </div>
+            <div >
+                <x-input-text
+                    wire:model="form.specification"
+                    name="title"
+                    label="Material Specification"
+                    placeholder="Enter material specification ..."
+                />
+            </div>
+        </div>
 
-        {{-- <x-radio label="Select Document Language" name="doc_type" :options="config('conf_documents.docTypes')" :selected="$this->form->doc_type"
-            wire:model="form.doc_type" />
 
-        <x-radio label="Select Document Type" name="language" :options="config('conf_documents.languages')" :selected="$this->form->language"
-            wire:model="form.language" /> --}}
-
-
-        <x-select label="Select Material Family" name="family" :options="$conf['families']" :selected="$this->form->family"
-            wire:model="form.family" />
-
-
-        <x-input-text wire:model="form.title" name="title" label="Document Title"
-            placeholder="Enter document title ..." />
-
-        <x-quill wire:model="form.remarks" label="Document Synopsis" name="remarks" :value="$this->form->remarks" />
+        <x-quill
+            wire:model="form.remarks"
+            label="Remarks/Notes"
+            name="remarks"
+            :value="$this->form->remarks"
+        />
 
         @if ($this->form->uid)
         <livewire:file-list :model="$this->form->document" collection="Material" label="Files" is_editable="true"/>
@@ -45,13 +62,13 @@
 
         <div class="flex justify-end my-4">
 
-            <a href="{{ $this->form->uid ? '/docs/'.$this->form->uid : '/docs' }}" class="text-white focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium text-sm px-5 py-2.5 me-2 mb-2 bg-red-700 hover:bg-red-800 p-2 rounded inline-flex items-center">
+            <a href="{{ $this->form->uid ? '/materials/'.$this->form->uid : '/materials' }}" class="text-white focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium text-sm px-5 py-2.5 me-2 mb-2 bg-red-700 hover:bg-red-800 p-2 rounded inline-flex items-center">
                 Cancel
             </a>
 
             <button type="submit"
                 class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2">
-                {{ $this->form->uid ? 'Update Document' : 'Add Document' }}
+                {{ $this->form->uid ? 'Update Material' : 'Add Material' }}
             </button>
         </div>
 

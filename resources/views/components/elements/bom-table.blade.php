5be0c41726ee6eff510a71da5dca067004a374f1
Kılıç Ali Temiz <katemiz@gmail.com>
2026-02-11 17:03:50 +0300

    stupid mistake corrected

--- resources/views/components/elements/bom-table.blade.php
+++ resources/views/components/elements/bom-table.blade.php
@@ -52,13 +52,13 @@
 
                                     @case('Detail')
                                         <a href="/details/Detail/view/{{$component->id}}" target="_blank">
-                                            {{ $component->part_number }}-{{ $item->config_number ?  $item->config_number .'-':''}} {{ $component->version }} 
+                                            {{ $component->part_number }}-{{ $component->config_number ?  $component->config_number .'-':''}} {{ $component->version }} 
                                         </a>
                                         @break
 
                                     @case('Assy')
                                         <a href="/products-assy/view/{{$component->id}}" target="_blank">
-                                            {{ $component->part_number }}-{{ $item->config_number ?  $item->config_number .'-':''}}{{ $component->version }} 
+                                            {{ $component->part_number }}-{{ $component->config_number ?  $component->config_number .'-':''}}{{ $component->version }} 
                                         </a>
                                         @break
 
